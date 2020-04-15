760b69b3d4fc5e08a62ef0de2cf5bd6e10c3724e
Adam Ostrowicki <adamostrowicki11@gmail.com>
2020-04-15 17:33:48 +0200

    -

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const scroll = new SmoothScroll('a[href*="#"]');
 
 const anims = document.querySelectorAll('.anim');
+const landing = document.querySelector('#landing');
+const nav = document.querySelector('nav');
 
-observerOptions={
+observerOptions = {
     threshold: 0.5,
-    
 };
 
-const observer = new IntersectionObserver(entries=>{
-    entries.forEach(entry=>{
-            if(entry.isIntersecting){
-                entry.target.classList.add('animate');
-            } 
+navOptions = {
+    threshold: 0.85,
+};
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
     });
 }, observerOptions);
 
-anims.forEach(anim=>{
+anims.forEach(anim => {
     observer.observe(anim);
 })
 
+
+const landing_observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            nav.classList.remove('scroll');
+        }
+        else {
+            nav.classList.add('scroll');
+        }
+    });
+}, navOptions);
+
+landing_observer.observe(landing);
 
 
